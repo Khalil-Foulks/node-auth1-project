e45176f43e41b9088e78e8bf3106ccd87a8f3050
Khalil <khalilfoulks@gmail.com>
2020-08-17 17:05:43 -0400

    finished users-model and auth endpoints

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const helmet = require('helmet');
 
 //Router import here
-// const usersRouter = require("../users/users-router.js");
-// const authRouter = require("../auth/auth-router.js");
+// const usersRouter = require("./users/users-router.js");
+const authRouter = require("./auth/authRouter");
 
 //Global Middleware here
 const server = express();
@@ -17,6 +17,6 @@ res.send(`<h2> Node auth 1 project is UP! </h2>`)
 })
 
 // server.use('/api/users', protected, usersRouter);
-// server.use('/api/auth', authRouter);
+server.use('/api/auth', authRouter);
 
 module.exports = server;
